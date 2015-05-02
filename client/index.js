cc7fa94aab2f53061ae7afd245fb7cb2104a5a84
Jose Zamudio <zamudio@outlook.com>
2015-05-01 21:26:44 -0700

    Add extra comments to frontend

--- client/index.js
+++ client/index.js
@@ -3,13 +3,16 @@
 angular.module('convert', [])
 .controller('ConverterCtrl', ['$scope', 'Convert', function($scope, Convert){
   $scope.convertInput = function() {
+    // Call factory and send input to the backend
     Convert.convert($scope.textInput)
     .then(function(response) {
+      // call downloadFile function with response from backend
       downloadFile(response.data.csv);
     });
   };
 
   function downloadFile(csv) {
+    //create  element with csv file to download
     var element = angular.element('<a/>');
     element.attr({
       href: 'data:attachment/csv;charset=utf-8,' + encodeURI(csv),
