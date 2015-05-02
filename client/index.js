21044736acd3bd3a830e384a6b5efe0de64efcf2
Jose Zamudio <zamudio@outlook.com>
2015-05-01 20:59:51 -0700

    Create file.csv and download

--- client/index.js
+++ client/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 angular.module('convert', [])
-.controller('ConverterCtrl', ['$scope', 'Convert', '$window', function($scope, Convert, $window){
+.controller('ConverterCtrl', ['$scope', 'Convert', function($scope, Convert){
   $scope.convertInput = function() {
     Convert.convert($scope.textInput)
     .then(function(response) {
-      console.log(response);
+      var element = angular.element('<a/>');
+      element.attr({
+        href: 'data:attachment/csv;charset=utf-8,' + encodeURI(response.data.csv),
+        target: '_blank',
+        download: 'file.csv'
+      })[0].click();
     });
   };
 }]);
--- test/unit/convert.js
+++ test/unit/convert.js
@@ -1,39 +1,18 @@
 'use strict';
 
 var expect = require('chai').expect;
-var _ = require('lodash');
 var Convert = require('../../server/models/convert');
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var describe = lab.describe;
 var it = lab.it;
-var beforeEach = lab.beforeEach;
 require('../../server/index');
 
 describe('Convert', function() {
   describe('convertToCSV', function() {
 
     it('should convert array of objects to csv', function(done) {
-      var objects = [
-        {
-          "name": "Adam",
-          "lastName" : "Smith",
-          "gender" : "male",
-          "country" : "Scotland"
-        },
-        {
-          "name": "George",
-          "lastName" : "Washington",
-          "gender" : "male",
-          "country" : "USA"
-        },
-        {
-          "name": "Marie",
-          "lastName" : "Curie",
-          "gender" : "female",
-          "country" : "France"
-        }
-      ]
+      var objects = '[ { "name": "Adam", "lastName" : "Smith", "gender" : "male", "country" : "Scotland" }, { "name": "George", "lastName" : "Washington", "gender" : "male", "country" : "USA" }, { "name": "Marie", "lastName" : "Curie", "gender" : "female", "country" : "France" }]';
       Convert.convertToCSV(objects, function(err, csv) {
         expect(err).to.not.be.ok;
         expect(csv).to.be.ok;
