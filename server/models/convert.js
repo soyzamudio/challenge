3adcdcc8b20993d989bfb21d3d36d5889916dfae
Jose Zamudio <zamudio@outlook.com>
2015-05-01 20:55:29 -0700

    Convert string to json and then to csv

--- server/models/convert.js
+++ server/models/convert.js
@@ -3,7 +3,14 @@
 var jsonexport = require('jsonexport');
 
 function convertToCSV(objects, cb) {
-
+  // Parse string into array of JSON Objects
+  var data = JSON.parse(objects);
+  jsonexport(data, function(err, csv){
+    // if error return callback with error = true
+    if (err) { return cb(true); }
+    // else return cb with csv format
+    return cb(null, csv);
+  });
 }
 
 module.exports = {convertToCSV:convertToCSV};
