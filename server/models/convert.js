685a1af18f5740273c2feca3812ab0eff5885c95
Jose Zamudio <zamudio@outlook.com>
2015-05-01 21:59:04 -0700

    Stop sorting of objects in backend model

--- server/models/convert.js
+++ server/models/convert.js
@@ -5,7 +5,11 @@ var jsonexport = require('jsonexport');
 function convertToCSV(objects, cb) {
   // Parse string into array of JSON Objects
   var data = JSON.parse(objects);
-  jsonexport(data, function(err, csv){
+
+  // No sorting of keys
+  var options = { orderHeaders: false }
+
+  jsonexport(data, options, function(err, csv){
     // if error return callback with error = true
     if (err) { return cb(true, null); }
     // else return cb with csv format
